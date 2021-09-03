843af9d85b16b728c65bf2bff54efa7828903000
Morris Lewis <42818296+mjlewis42@users.noreply.github.com>
2021-09-03 11:27:15 -0400

    Update Project4.js

--- Project/Project4.js
+++ Project/Project4.js
@@ -1,6 +1,4 @@
 /*
-Project 1
-
 Program button notes:
 	Each button performs a different movement.  Hit reset after each button finalizes or play them in order from 1 to 4 when they finish their movements for a small story.
 */
